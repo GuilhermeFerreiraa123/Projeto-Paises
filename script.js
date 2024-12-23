08a8fa7463b61cdce830c289e87aacae83cc177f
MiguelRibeiro <2241595@my.ipleiria.com>
2024-12-23 14:45:05 +0000

    Modificações na API e afins

--- script.js
+++ script.js
@@ -1,21 +1,80 @@
-fetch('https://restcountries.com/v3.1/all')
-  .then(response => response.json())
-  .then(countries => {
-    const countriesListDiv = document.getElementById('countries-list');
-    const template = document.getElementById('country-card-template');
+var cloneOriginalCard = $('#country-card-template').html();
+var erro_message = document.getElementById('Erro-Message');
+var card_info = document.getElementsByClassName('card-info-paises');
 
-    // Renderiza todos os países
-    countries.forEach(country => {
-      // Clona o conteúdo do template
-      const clone = template.content.cloneNode(true);
+$(document).ready(function () {
+  carregarTodosPaises();
+});
 
-      // Preenche os dados do país
-      clone.querySelector('img').src = country.flags.png;
-      clone.querySelector('img').alt = Bandeira de ${country.name.common};
-      clone.querySelector('.card-title').textContent = country.name.common;
-      clone.querySelector('.card-text').textContent = Região: ${country.region};
+function carregarTodosPaises() {
+  $('#countries-list').html('');
 
-      // Adiciona o clone ao container
-      countriesListDiv.appendChild(clone);
-    });
+  $.ajax({
+    method: 'GET',
+    url: 'https://restcountries.com/v3.1/all'
+  }).done(function (dados) {
+    exibirPaises(dados);
+  }).fail(function () {
+    alert('Erro ao buscar informações dos países.');
   });
+}
+
+$('#btn-search').on('click', function () {
+  var pesquisa = $('#titulo').val().trim();
+
+  if (pesquisa === '') {
+    carregarTodosPaises();
+    return;
+  }
+
+  $('#countries-list').html('');
+
+  $.ajax({
+    method: 'GET',
+    url: 'https://restcountries.com/v3.1/name/' + pesquisa
+  }).done(function (dados) {
+    exibirPaises(dados);
+  }).fail(function () {
+
+    erro_message.style.display = 'block';
+  });
+});
+
+function exibirPaises(paises) {
+  for (var i = 0; i < paises.length; i++) {
+    var pais = paises[i];
+    var cloneCard = $(cloneOriginalCard);
+
+    $('.card-title', cloneCard).text(pais.name.common);
+    $('.card-img-top', cloneCard).attr("src", pais.flags.png);
+    $('.card-body', cloneCard).append('<p>Continente: ' + pais.region + '</p>');
+    erro_message.style.display = 'none';
+
+    var objectPais = {
+      "nome": pais.name.common,
+      "populacao": pais.population,
+      "bandeira": pais.flags.png,
+      "continente": pais.region
+    };
+
+    var stringObjectPais = JSON.stringify(objectPais);
+    cloneCard.find('.btn-favoritos').attr("onclick", "addfavoritos(" + stringObjectPais + ")");
+
+    $('#countries-list').append(cloneCard);
+  }
+}
+
+function addfavoritos(pais) {
+  var arrayfavoritos;
+  
+  if (localStorage.getItem("pais") === null) {
+    arrayfavoritos = [];
+  } else {
+    arrayfavoritos = JSON.parse(localStorage.getItem("pais"));
+  }
+
+  arrayfavoritos.push(pais);
+
+  var favoritosstorage = JSON.stringify(arrayfavoritos);
+  localStorage.setItem("pais", favoritosstorage);
+}
