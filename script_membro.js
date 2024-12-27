76ffdb36c7e03738c1eea4ed3e3b5b983a6a4a18
MiguelRibeiro <2241595@my.ipleiria.com>
2024-12-27 20:21:57 +0000

    Modificação da pagina Sobre Nos

--- script_membro.js
+++ script_membro.js
@@ -1,4 +1,10 @@
+var info_guilherme = document.getElementById('show-info-guilherme');
+var info_miguel = document.getElementById('show-info-miguel');
+var info_tiago = document.getElementById('show-info-tiago');
 
+var title_guilherme = document.getElementById('title-guilherme');
+var title_miguel = document.getElementById('title-miguel');
+var title_tiago = document.getElementById('title-tiago');
 
 
 $(document).ready(function () {
@@ -7,7 +13,25 @@ $(document).ready(function () {
 
 
 function naocarregainfo() {
+    info_guilherme.innerHTML = '';
+    info_miguel.innerHTML = '';
+    info_tiago.innerHTML = '';
+}
 
+function show_guilherme(){
+    info_guilherme.innerHTML = '<h3 class="info-membros-text-title">Guilherme Ferreira</h3> <br> <h4 class="info-membros-text"><b>Estudante no IPL   </b></h4> <h4 class="info-membros-text"><b>Número do Estudante: </b>2241869</h4> <h4 class="info-membros-text"><b>Curso do Estudante: </b>TeSP - Programação de Sistemas de Informação</h4>';
+    title_guilherme.innerHTML = '';
+    title_guilherme.style.setProperty('margin-top', '20%', 'important');
+}
 
+function show_miguel(){
+    info_miguel.innerHTML = '<h3 class="info-membros-text-title">Miguel Ribeiro</h3><br><h4 class="info-membros-text"><b>Estudante no IPL   </b></h4><h4 class="info-membros-text"><b>Número do Estudante: </b>2241595</h4><h4 class="info-membros-text"><b>Curso do Estudante: </b>TeSP - Programação de Sistemas de Informação</h4>';
+    title_miguel.innerHTML = '';
+    title_miguel.style.setProperty('margin-top', '20%', 'important');
+}
 
+function show_tiago(){
+    info_tiago.innerHTML = '<h3 class="info-membros-text-title">Tiago Carmo</h3><br><h4 class="info-membros-text"><b>Estudante no IPL   </b></h4><h4 class="info-membros-text"><b>Número do Estudante: </b>2241610</h4><h4 class="info-membros-text"><b>Curso do Estudante: </b>TeSP - Programação de Sistemas de Informação</h4>';
+    title_tiago.innerHTML = '';
+    title_tiago.style.setProperty('margin-top', '20%', 'important');
 }
