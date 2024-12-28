c203124d0f6650796dd0e1977060188cf0d9c59a
MiguelRibeiro <2241595@my.ipleiria.com>
2024-12-28 18:05:36 +0000

    Modificação final na aba das informações do membros, retoques nas navbars para a hiperligação do botao Contactos

--- script_membro.js
+++ script_membro.js
@@ -2,10 +2,9 @@ var info_guilherme = document.getElementById('show-info-guilherme');
 var info_miguel = document.getElementById('show-info-miguel');
 var info_tiago = document.getElementById('show-info-tiago');
 
-var title_guilherme = document.getElementById('title-guilherme');
-var title_miguel = document.getElementById('title-miguel');
-var title_tiago = document.getElementById('title-tiago');
-
+var seta_guilherme = document.getElementById('seta-guilherme');
+var seta_miguel = document.getElementById('seta-miguel');
+var seta_tiago = document.getElementById('seta-tiago');
 
 $(document).ready(function () {
     naocarregainfo();
@@ -13,25 +12,40 @@ $(document).ready(function () {
 
 
 function naocarregainfo() {
-    info_guilherme.innerHTML = '';
-    info_miguel.innerHTML = '';
-    info_tiago.innerHTML = '';
+    info_guilherme.hidden = true;
+    info_miguel.hidden = true;
+    info_tiago.hidden = true;
+    seta_guilherme.innerHTML = '<li class="bold-text" onclick="show_guilherme()">Guilherme <i id="seta-guilherme" class="bi bi-arrow-right-short seta-membros"></i></li>';
+    seta_miguel.innerHTML = '<li class="bold-text" onclick="show_miguel()">Miguel <i id="seta-miguel" class="bi bi-arrow-right-short seta-membros"></i></li>';
+    seta_tiago.innerHTML = '<li class="bold-text" onclick="show_tiago()">Tiago <i id="seta-tiago" class="bi bi-arrow-right-short seta-membros"></i></li>';
 }
 
 function show_guilherme(){
-    info_guilherme.innerHTML = '<h3 class="info-membros-text-title">Guilherme Ferreira</h3> <br> <h4 class="info-membros-text"><b>Estudante no IPL   </b></h4> <h4 class="info-membros-text"><b>Número do Estudante: </b>2241869</h4> <h4 class="info-membros-text"><b>Curso do Estudante: </b>TeSP - Programação de Sistemas de Informação</h4>';
-    title_guilherme.innerHTML = '';
-    title_guilherme.style.setProperty('margin-top', '20%', 'important');
+    info_guilherme.hidden = false;
+    seta_guilherme.innerHTML = '<li class="bold-text" onclick="hide_guilherme()"><b>Guilherme </b><i id="seta-guilherme" class="bi bi-arrow-left-short seta-membros"></i></li>';
 }
 
 function show_miguel(){
-    info_miguel.innerHTML = '<h3 class="info-membros-text-title">Miguel Ribeiro</h3><br><h4 class="info-membros-text"><b>Estudante no IPL   </b></h4><h4 class="info-membros-text"><b>Número do Estudante: </b>2241595</h4><h4 class="info-membros-text"><b>Curso do Estudante: </b>TeSP - Programação de Sistemas de Informação</h4>';
-    title_miguel.innerHTML = '';
-    title_miguel.style.setProperty('margin-top', '20%', 'important');
+    info_miguel.hidden = false;
+    seta_miguel.innerHTML = '<li class="bold-text" onclick="hide_miguel()"><b>Miguel </b><i id="seta-miguel" class="bi bi-arrow-left-short seta-membros"></i></li>';
 }
 
 function show_tiago(){
-    info_tiago.innerHTML = '<h3 class="info-membros-text-title">Tiago Carmo</h3><br><h4 class="info-membros-text"><b>Estudante no IPL   </b></h4><h4 class="info-membros-text"><b>Número do Estudante: </b>2241610</h4><h4 class="info-membros-text"><b>Curso do Estudante: </b>TeSP - Programação de Sistemas de Informação</h4>';
-    title_tiago.innerHTML = '';
-    title_tiago.style.setProperty('margin-top', '20%', 'important');
+    info_tiago.hidden = false;
+    seta_tiago.innerHTML = '<li class="bold-text" onclick="hide_tiago()"><b>Tiago </b><i id="seta-tiago" class="bi bi-arrow-left-short seta-membros"></i></li>';
+}
+
+function hide_guilherme(){
+    seta_guilherme.innerHTML = '<li class="bold-text" onclick="show_guilherme()">Guilherme <i id="seta-guilherme" class="bi bi-arrow-right-short seta-membros"></i></li>';
+    info_guilherme.hidden = true;
+}
+
+function hide_miguel(){
+    seta_miguel.innerHTML = '<li class="bold-text" onclick="show_miguel()">Miguel <i id="seta-miguel" class="bi bi-arrow-right-short seta-membros"></i></li>';
+    info_miguel.hidden = true;
+}
+
+function hide_tiago(){
+    seta_tiago.innerHTML = '<li class="bold-text" onclick="show_tiago()">Tiago <i id="seta-tiago" class="bi bi-arrow-right-short seta-membros"></i></li>';
+    info_tiago.hidden = true;
 }
